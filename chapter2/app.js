2ddf512352474c2643a55e85ee4d56ee606e4ecd
TAMHEED AAKAS SAMAR <139570438+tamheedaakas@users.noreply.github.com>
2023-09-10 17:03:46 +0500

    Add files via upload

---
+++ chapter2/app.js
@@ -0,0 +1,33 @@
+// // Exercise 1
+// var username = "tamheed"
+// alert(username)
+
+// // Exercise 2
+// var myName = "TAMHEED AAKAS SAMAR";
+// console.log(myName)
+
+
+// // Exercise 3
+
+// var message = "Hello World";
+// alert(message);
+
+// Exercise 4
+
+// var name1 = "Tamheed";
+// var age = 22;
+// var program = "Certified Mobile and Web Application Development";
+
+// // Display bio data in alert boxes
+// alert("Name: " + name1);
+// alert("Age: " + age);
+// alert("Program: " + program);
+
+// Exercise 5
+var pizzaWord = "PIZZA";
+alert(pizzaWord + "\n" + pizzaWord.slice(0, 4) + "\n" + pizzaWord.slice(0, 3) + "\n" + pizzaWord.slice(0, 2) + "\n" + pizzaWord.slice(0, 1));
+
+// Exercise 6
+
+ var email = "tamheed.samar@gmail.com";
+ alert("My email address is: " + email);
