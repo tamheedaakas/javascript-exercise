b031725c0ab36500634813167dd5b3f4b2e79f7e
TAMHEED AAKAS SAMAR <139570438+tamheedaakas@users.noreply.github.com>
2023-09-10 17:05:31 +0500

    Add files via upload

---
+++ chapter4/app.js
@@ -0,0 +1,12 @@
+// exercise 1
+var variable1, variable2, variable3;
+var variable1 = 10, variable2 = "Hello", variable3 = true;
+console.log(variable1 , variable2 , variable3)
+
+// exercise 2
+
+
+
+
+
+// exercise 1
